88cc869322ae52ac25cd5be235901f3f85c5659a
JoaombCoelho <jaomanel6@gmail.com>
2024-07-28 22:38:06 -0300

    Atualização de commit

---
+++ Tipo_Variaveis1/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        
+    
+            concatenacao = 1+1+1+"1";
+            System.out.println(concatenacao);  
+            
+            
+    }
+}
--- Tipo_Variaveis1/src/Tipo_Variaveis.java
+++ Tipo_Variaveis1/src/Tipo_Variaveis.java
@@ -1,5 +1,10 @@
 public class Tipo_Variaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+            int idade; //Tipo "int", nome "idade", com nenhum valor atribuído. 
+            int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
+            double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
+
+    
     }
 }

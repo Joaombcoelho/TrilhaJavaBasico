357b127cc14bd6801b9e21aa7abcbf6f4d525643
JoaombCoelho <jaomanel6@gmail.com>
2024-07-29 22:36:10 -0300

    Update MinhaClasse_AboutMe_Usuario

--- Tipo_Variaveis1/src/SmartTv/Usuario.java
+++ Tipo_Variaveis1/src/SmartTv/Usuario.java
@@ -9,6 +9,11 @@ public class Usuario {
         System.out.println("Volume Atual ? " + smartTv.volume);
         System.out.println("Volume Atual : " + smartTv.canal);
 
-        
+        smartTv.ligar();
+        System.out.println("Novo Status - Tv Ligada?" + smartTv.ligada);
+        smartTv.Desligar();
+        System.out.println("Tv Ligada?" + smartTv.ligada);
+
+
     }
 }
